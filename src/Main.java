f67404b52dc03b502c2f5c5aa4b9dceb1f582d4a
kosenin <kosenin@yandex.ru>
2019-11-21 14:30:42 +0100

    Corrected output

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 import java.math.BigInteger;
 
 public class Main {
-    
+
+
     public static BigInteger SquirrelFactorial(int n) {
         BigInteger fact = BigInteger.ONE;
         for (int i = 1; i <= n; i++) {
             fact = fact.multiply(BigInteger.valueOf(i));
-            System.out.println(fact);
         }
         char[] smaragd = String.valueOf(fact).toCharArray();
 
@@ -15,5 +15,4 @@ public class Main {
     }
 
 }
-
 
